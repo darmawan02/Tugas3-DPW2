eb3c5c0e358d54001faf11337f75f3f0d9b1b40d
darmawan02 <107595676+darmawan02@users.noreply.github.com>
2022-09-06 22:36:29 +0700

    Create Tugas3

---
+++ Tugas_3/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/shop', function () {
+    return view('shop');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('template', function () {
+    return view('template.base');
+});
+Route::get('beranda', function () {
+    return view('beranda');
+});
+Route::get('produk', function () {
+    return view('produk');
+});
+Route::get('kategori', function () {
+    return view('kategori');
+});
+Route::get('login', function () {
+    return view('login');
+});
